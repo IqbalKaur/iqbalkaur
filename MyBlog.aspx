c28d4f6ad0705c51aaccaaec817c23b73fce69a1
Iqbal Kaur <cuteiqbal87@gmail.com>
2015-01-06 00:22:08 -0500

    MyBlog page created.

---
+++ MyBlog.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MyBlog.aspx.cs" Inherits="MyBlog" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    Post Title: <asp:TextBox ID="txtpostTitle" runat="server"></asp:TextBox><br />
+        Post SubTitle: <asp:TextBox ID="txtpostSubTitle" runat="server"></asp:TextBox><br />
+        Posted By: <asp:TextBox ID="txtpostedBy" runat="server"></asp:TextBox><br />
+        Date: <asp:TextBox ID="txtCalender" runat="server" TextMode="DateTime"></asp:TextBox><br />
+        Content:<asp:TextBox ID="txtcontent" runat="server" TextMode="MultiLine"></asp:TextBox><br /><br />
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ MyBlog.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class MyBlog : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            con.Open();
+        }
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "INSERT into BlogTB(postTitle, postSubTitle, postedBy, date, content) VALUES(@postTitle, @postSubTitle, @postedBy, @date, @content)";
+        cmd.Parameters.Add("@postTitle", SqlDbType.NVarChar).Value = txtpostTitle.Text;
+        cmd.Parameters.Add("@postSubTitle", SqlDbType.NVarChar).Value = txtpostSubTitle.Text;
+        cmd.Parameters.Add("@postedBy", SqlDbType.VarChar).Value = txtpostedBy.Text;
+        cmd.Parameters.Add("@date", SqlDbType.Date).Value = txtCalender.Text; 
+        cmd.Parameters.Add("@content", SqlDbType.NVarChar).Value = txtcontent.Text;
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+    }
+}
