c47c8006e83288a70abbda46aef066ffc4cf2bcd
Iqbal Kaur <cuteiqbal87@gmail.com>
2015-01-24 18:30:26 -0500

    tinyMCE image upload using tinyfilemanager.net http://www.andyarndt.net/TinyFileManager.aspx

--- js/tinymceEditor.js
+++ js/tinymceEditor.js
@@ -1,4 +1,6 @@
-﻿
+﻿// tfm_path tells the provided tinymce plugins
+// where to find dialog.aspx, which is the file manager webpage
+tfm_path = '/lib/tinyfilemanager.net';  
 tinymce.init({
     selector: ".mcetiny",
     theme: "modern",
@@ -6,7 +8,7 @@ tinymce.init({
         "advlist autolink lists link image charmap print preview hr anchor pagebreak",
         "searchreplace wordcount visualblocks visualchars code fullscreen",
         "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor"
+        "emoticons template paste textcolor tinyfilemanager.net"
     ],
     toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
     toolbar2: "print preview media | forecolor backcolor emoticons",
